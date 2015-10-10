9ea5f3a08f3923cf29631d60879b2cdf033fdca3
nchristiny <icarus747@gmail.com>
2015-10-10 16:24:23 -0500

    Add bcrypt to env and gemfile

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,9 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 # Require gems we care about
 require 'rubygems'
 
+require 'bcrypt'
+require 'faker'
+
 require 'uri'
 require 'pathname'
 
@@ -19,8 +22,6 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 require 'erb'
-require 'bcrypt'
-require 'faker'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
