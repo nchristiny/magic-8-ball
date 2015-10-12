2024e521dc76d7d27f932d78a8f842ce18390c0a
nchristiny <icarus747@gmail.com>
2015-10-11 23:36:56 -0500

    Finish answers CRUD

--- app/controllers/balls.rb
+++ app/controllers/balls.rb
@@ -5,17 +5,30 @@ get '/balls' do
 end
 
 get '/balls/new' do
-  redirect_guests
   current_user
   erb :"/balls/new"
+end
+
+post '/balls/new' do
+  @user = current_user
+  @ball = @user.balls.new(params[:ball])
+  if @ball.save
+    redirect "/balls/#{@ball.id}"
+  else
+    @errors = @ball.errors.full_messages
+    erb :"balls/new"
+  end
 end
 
 get '/balls/:id/result' do
   @ball = Ball.find(params[:id])
   user_id = @ball.user_id
   @author = User.find(user_id)
-
-  erb :"balls/result"
+  if @ball.answers.count == 0
+    redirect "balls/#{@ball.id}/answers"
+  else
+    erb :"balls/result"
+  end
 end
 
 get '/balls/:id' do
@@ -23,6 +36,31 @@ get '/balls/:id' do
   @ball = Ball.find(params[:id])
   user_id = @ball.user_id
   @author = User.find(user_id)
-
   erb :"balls/show"
+end
+
+put '/balls/:id' do
+  @ball = Ball.find(params[:id])
+  @ball.assign_attributes(params[:ball])
+  if @ball.save
+    redirect "balls/#{@ball.id}"
+  else
+    @errors = @entry.errors.full_messages
+    erb :"balls/:id/edit"
+  end
+end
+
+get '/balls/:id/edit' do
+  @ball = Ball.find(params[:id])
+  user_id = @ball.user_id
+  @author = User.find(user_id)
+  redirect '/' unless @author == current_user
+  erb :"balls/edit"
+end
+
+delete '/balls/:id' do
+  redirect_guests
+  ball = Ball.find(params[:id])
+  ball.destroy
+  redirect '/balls'
 end
